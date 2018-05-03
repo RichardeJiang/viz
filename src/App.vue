0439c636e3cf7c8f41b0dfaad2a719d85e75ba95
richardejiang <richardejiang@hotmail.com>
2018-05-03 18:59:57 +0800

    tested with uploads

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,30 @@
         <img src="./assets/logo.png" style="vertical-align:middle;">
       </div>
       <router-link to="/">
-          <el-button @click="startHacking" type="primary">Star</el-button>
-        </router-link>
-        <router-link to="/add">
-          <el-button type="selection">End</el-button>
-        </router-link>
-        <router-link to="/ec">
-          <el-button type="success">EC</el-button>
-        </router-link>
+        <el-button @click="startHacking" type="primary">Star</el-button>
+      </router-link>
+      <router-link to="/add">
+        <el-button type="selection">End</el-button>
+      </router-link>
+      <router-link to="/ec">
+        <el-button type="success">EC</el-button>
+      </router-link>
+      <div style="line-height:20px;margin-left:10px; margin-bottom: 10px" align="left">
+        <el-upload
+          class="upload-demo"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :before-remove="beforeRemove"
+          :on-success="handleSuccess"
+          multiple
+          :limit="3"
+          :on-exceed="handleExceed"
+          :file-list="fileList">
+          <el-button size="small" type="primary">Click to upload</el-button>
+          <div slot="tip" class="el-upload__tip" style="margin-left:5px">jpg/png files with a size less than 500kb</div>
+        </el-upload>
+      </div>
     </el-aside>
     <el-container>
       <el-header>Header</el-header>
@@ -36,6 +52,11 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+        fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+      };
+  },
   methods: {
     startHacking () {
       this.$notify({
@@ -44,6 +65,28 @@ export default {
         message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
         duration: 2500
       })
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleSuccess(response, file, fileList) {
+      console.log(file);
+      var fileName = file[0].name;
+      this.$notify({
+        title: 'OK!',
+        type: 'success',
+        message: 'The file ' + fileName,
+        duration: 2000
+      })
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(`The limit is 3, you selected ${files.length} files this time, add up to ${files.length + fileList.length} totally`);
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`Sure to remove ${ file.name }？`);
     }
   }
 }
